ddfadd9f6fbf2a1ebb568bcd97f0cbbba9e736bd
dotnet <nilesh.soni@brightcodess.com>
2020-02-21 17:39:07 +0530

    Initial commit

---
+++ DemoGithub/DemoGithub/Models/Demo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoGithub.Models
+{
+    public class Demo
+    {
+        public int demoid { get; set; }
+        public string Demo_name { get; set; }
+        public string Demo_Father_name { get; set; }
+    }
+}
